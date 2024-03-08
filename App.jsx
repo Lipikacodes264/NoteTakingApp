96c0432386ea3d55d6bb3c9ad08cf2c21d4625ee
Lipikacodes264 <134603113+Lipikacodes264@users.noreply.github.com>
2024-03-08 22:18:04 +0530

    commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Header
+ from "./Components/Header";
+ import Notes from "./Components/Notes";
+function App() {
+  return (
+     <>
+       <Header />
+       <Notes />
+       </>
+  );
+}
+
+export default App;
